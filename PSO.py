01d6744a45375a329842b66fd052085b7cd3c110
Nathan <natlivsey@gmail.com>
2019-11-24 18:14:11 +0000

    Very basic but working PSO, experiment with removing last 5 values?

--- PSO.py
+++ PSO.py
@@ -1,4 +1,6 @@
 import random
+import time
+
 class Particle:
     def __init__(self,training):
         self.TrainingValues = training
@@ -6,9 +8,9 @@ class Particle:
         self.velocity = []
         self.generateVelocity()
         self.personalBest= self.weights[:]
-        self.personalBestValue =self.evaluateWeights(self.personalBest)
-        intertia = 0.721
-        attraction = 1.1193
+        self.personalBestValue =self.evaluateWeights()
+        self.intertia = 0.721
+        self.attraction = 1.1193
 
     def generateVelocity(self):
         temporaryValues=self.populateValues()
@@ -29,15 +31,63 @@ class Particle:
             total += (day[i]*values[i-1])
         return abs((total - day[0]))
 
-    def evaluateWeights(self,values):
+    def evaluateWeights(self):
         errors = []
         for i in range (len(self.TrainingValues)):
-            errors.append(self.evaluateDay(i,values))
+            errors.append(self.evaluateDay(i,self.weights))
         errors = sum(errors)
         return(errors)
 
-    def tick(self):
+    def tick(self,globalBest):
         newPosition = []
+        for i in range(len(self.weights)):
+            newPosition.append(self.weights[i]+self.velocity[i])
+
+        interialVelocity = []
+        for i in range (len(self.weights)):
+            interialVelocity.append(self.velocity[i]*self.intertia)
+
+        pBestVelocity=[]
+        for i in range (len(self.weights)):
+            pBestVelocity.append((self.personalBest[i]-self.weights[i])*self.attraction)
+
+        gBestVelocity=[]
+        for i in range (len(self.weights)):
+            gBestVelocity.append((globalBest[i]-self.weights[i])*self.attraction)
+
+        newVelocity=[]
+        for i in range (len(self.velocity)):
+            newVelocity.append(interialVelocity[i]+pBestVelocity[i]+gBestVelocity[i])
+
+        self.weights = newPosition[:]
+        self.velocity = newVelocity[:]
+
+        evaluation = self.evaluateWeights()
+        if(evaluation<self.personalBestValue):
+            self.personalBest = self.weights[:]
+            self.personalBestValue=evaluation
+
+class Swarm:
+    def __init__(self,training,swarmSize):
+        self.swarm = []
+        self.globalBest=[]
+        self.globalBestValue = 100000000000000
+        for i in range(swarmSize):
+            p=Particle(training)
+            self.swarm.append(p)
+            evaluation=p.evaluateWeights()
+            if(evaluation<self.globalBestValue):
+                self.globalBest = p.personalBest[:]
+                self.globalBestValue = evaluation
+
+    def tick(self):
+        for x in self.swarm:
+            x.tick(self.globalBest)
+            if x.personalBestValue<self.globalBestValue:
+                self.globalBest = x.personalBest[:]
+                self.globalBestValue = x.personalBestValue
+                print(self.globalBestValue)
+
 
 def readFromFile(file):
     out=[]
@@ -53,8 +103,12 @@ def readFromFile(file):
 def main():
     trainFile = readFromFile("cwk_train.csv")
     testFile = readFromFile("cwk_test.csv")
-    particle = Particle(trainFile)
-
+    swarm = Swarm(trainFile,130)
+    timeout=time.time()+60
+    for i in range(260):
+        swarm.tick()
+    print(swarm.globalBestValue)
+    print(swarm.globalBest)
 
 if __name__== "__main__":
     main()

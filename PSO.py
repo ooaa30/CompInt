cf10f4d8c42fc541c0c8e37e380b87360dd7edc8
Nathan <natlivsey@gmail.com>
2019-11-24 16:37:52 +0000

    Skeleton of particle for coursework PSO

--- PSO.py
+++ PSO.py
@@ -2,28 +2,43 @@ import random
 class Particle:
     def __init__(self,training):
         self.TrainingValues = training
-        self.weights = None
-        self.populateValues()
+        self.weights = self.populateValues()
+        self.velocity = []
+        self.generateVelocity()
+        self.personalBest= self.weights[:]
+        self.personalBestValue =self.evaluateWeights(self.personalBest)
+        intertia = 0.721
+        attraction = 1.1193
+
+    def generateVelocity(self):
+        temporaryValues=self.populateValues()
+        for i in range(len(self.weights)):
+            self.velocity.append(self.weights[i]-temporaryValues[i]/2)
 
     def populateValues(self):
-        self.weights = [0 for i in range (len(self.TrainingValues[1])-1)]
-        for i in range(len(self.weights)):
-            self.weights[i] = random.uniform(0.0,1.0)
+        values = [0 for i in range (len(self.TrainingValues[1])-1)]
+        for i in range(len(values)):
+            values[i] = random.uniform(0.0,1.0)
+        return (values[:])
 
-    def evaluateDay(self,day):
+    def evaluateDay(self,day,values):
         total = 0
         day = self.TrainingValues[day]
         test = 0;
         for i in range(1,len(day)):
-            total += (day[i]*self.weights[i-1])
+            total += (day[i]*values[i-1])
         return abs((total - day[0]))
 
-    def evaluateWeights(self):
+    def evaluateWeights(self,values):
         errors = []
         for i in range (len(self.TrainingValues)):
-            errors.append(self.evaluateDay(i))
+            errors.append(self.evaluateDay(i,values))
         errors = sum(errors)
-        print(errors)
+        return(errors)
+
+    def tick(self):
+        newPosition = []
+
 
 def readFromFile(file):
     out=[]
